41a4f41aa258eabcfad9315db7c7f19ee78feb60
lispro06@gmail.com <lispro06@gmail.com@cef682b6-320a-dce6-23e8-3da3ab6ae5dc>
2011-09-21 08:05:09 +0000

    'vicolochurch'

---
+++ trunk/src/com/paxmodept/demo/IconTextListAdapter.java
@@ -0,0 +1,71 @@
+package com.paxmodept.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class IconTextListAdapter extends BaseAdapter {
+
+	private Context mContext;
+
+	private List<IconTextItem> mItems = new ArrayList<IconTextItem>();
+
+	public IconTextListAdapter(Context context) {
+		mContext = context;
+	}
+
+	public void addItem(IconTextItem it) {
+		mItems.add(it);
+	}
+
+	public void setListItems(List<IconTextItem> lit) {
+		mItems = lit;
+	}
+
+	public int getCount() {
+		return mItems.size();
+	}
+
+	public Object getItem(int position) {
+		return mItems.get(position);
+	}
+
+	public boolean areAllItemsSelectable() {
+		return false;
+	}
+
+	public boolean isSelectable(int position) {
+		try {
+			return mItems.get(position).isSelectable();
+		} catch (IndexOutOfBoundsException ex) {
+			return false;
+		}
+	}
+
+	public long getItemId(int position) {
+		return position;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		IconTextView itemView;
+		
+		//convertView가 한번만 생성됨으로 성능상으로 좋은 소스 ~
+		if (convertView == null) {
+			itemView = new IconTextView(mContext, mItems.get(position));
+		} else {
+			itemView = (IconTextView) convertView;
+			
+			itemView.setIcon(mItems.get(position).getIcon());
+			itemView.setText(0, mItems.get(position).getData(0));
+			itemView.setText(1, mItems.get(position).getData(1));
+			itemView.setText(2, mItems.get(position).getData(2));
+		}
+
+		return itemView;
+	}
+
+}
